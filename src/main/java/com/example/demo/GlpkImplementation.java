f76910a42f1d00456d3bc059e7b7a05bdb956366
betogontijo <maximoussk8@yahoo.com.br>
2017-11-14 20:13:41 -0200

    stable commit

--- src/main/java/com/example/demo/GlpkImplementation.java
+++ src/main/java/com/example/demo/GlpkImplementation.java
@@ -1,58 +1,17 @@
 package com.example.demo;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 
 import org.gnu.glpk.GLPK;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import br.com.betogontijo.nativeutils.NativeUtils;
+import br.com.betogontijo.nativeutils.GlpkLoader;
 
 public class GlpkImplementation {
 
 	public GlpkImplementation() {
-		PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		String prefix = "/static/glpk-4.63/w64/";
-		try {
-			for (Resource resource : resolver.getResources("classpath*:" + prefix + "*")) {
-				try {
-					String library = prefix + resource.getFilename();
-					if (library.endsWith(".dll")) {
-						NativeUtils.loadLibraryFromJar(library);
-					}
-				} catch (IOException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// try {
-		// String path =
-		// getClass().getResource("/static/glpk-4.63/w64/").getPath().replaceAll("/",
-		// "\\\\");
-		// path = path.startsWith("\\") ? path.substring(1) : path;
-		// System.setProperty("java.library.path", path + ";" +
-		// System.getProperty("java.library.path"));
-		// addLibraryPath(path);
-		// } catch (Exception e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// String pathToAdd =
-		// context.getResource("classpath:static/libglpk-java-1.10.0/swig/.libs/").getURI().toString();
-		// System.out.println(pathToAdd);
-		// try {
-		// addLibraryPath(pathToAdd);
-		// } catch (Exception e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+		GlpkLoader.load();
 	}
 
 	public void runSimplex() {
